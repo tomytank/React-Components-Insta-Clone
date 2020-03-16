836e7f37d1013f9ba9155c2ff439c0d37453aa74
tomytank <github@iamtom.com>
2020-03-16 19:27:52 +0100

    Corrected return statement with a single element returned avoiding JSX fragments

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ PostsContainer components and the CommentSection Components.
 Not all files need code added. 
 Look at each file to see where you need to pass props or add code 
 */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import SearchBarContainer from "./components/SearchBar/SearchBarContainer";
 import PostsPage from "./components/PostsContainer/PostsPage";
@@ -16,8 +16,10 @@ const App = () => {
   return (
     <div className="App">
       {/* Add imported components here to render them */
-      <SearchBarContainer />
-      <PostsPage />
+        <div>
+          <SearchBarContainer />
+          <PostsPage />
+        </div>
       }
     </div>
   );
