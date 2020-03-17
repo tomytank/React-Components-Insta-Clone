c45cab5f02e2b89eb27b912d8c0669fb14555851
tomytank <github@iamtom.com>
2020-03-17 13:24:40 +0100

    Changing post state variable

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -7,9 +7,9 @@ import PostHeader from "./PostHeader";
 import "./Posts.css";
 
 // pass props in this file to
-const Post = props => {
+const Post = props() => {
   // set up state for the likes
-const [linkeState, setLikeState] = useState(likeData);
+//const [likeState, setLikeState] = useState();
   return (
     <div className="post-border">
       <PostHeader
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -11,10 +11,12 @@ const PostsPage = () => {
   const [postData, setPostData] = useState (instaData);
   console.log(instaData);
   console.log(useState);
+  console.log(postData);
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */
-      <Post data = {postData} />
+      //<Post data = {postData} />
+      
       }
     </div>
   );
