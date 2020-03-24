a0c7b397a0bc4cf1d4630a1d9ab590fd0e5ec245
tomytank <github@iamtom.com>
2020-03-24 13:14:58 +0100

    Added timestamp to comment section

--- src/components/CommentSection/Date.js
+++ src/components/CommentSection/Date.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React from "react";
 
 
 
@@ -6,7 +6,7 @@ import React,{useState} from "react";
 const Date = props => {
     const styleDate = {marginLeft: "10px", textAlign:"left",lineHeight:"35px"}
     console.log("The props in Date.js ", props);
-    const [date, setDate] = useState(props);
+    //const [date, setDate] = useState(props);
     return (
         <div style={styleDate}>{props.timestamp}</div>
     );
